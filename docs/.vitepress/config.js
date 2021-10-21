3b4a455e76a22febca23cf5f19797ea3202233bb
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-21 17:03:06 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,10 +2,14 @@ module.exports = {
   title: "Football API",
   description: "An API for historic football data.",
   lang: "en-GB",
-  head: [
-    [
-      "link",
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-    ],
-  ],
+  head: [["link", { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]],
+
+  themeConfig: {
+    repo: "JMaylor/football-fastify-api",
+    docsDir: "docs",
+
+    editLinks: true,
+    editLinkText: "Edit on GitHub",
+    lastUpdated: "Last Updated",
+  },
 };
